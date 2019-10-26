4427c06980483e377ddae4ce0f9c63a0b87b6c4d
Christopher Pham <c1pham@student.bridgew.edu>
2019-10-26 13:13:43 -0400

    create account; TODO incorporate server in feature

--- Stock_Project/routes/index.js
+++ Stock_Project/routes/index.js
@@ -10,4 +10,8 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Log Into Your Account' });
 });
 
+router.get('/newuser', function(req, res, next) {
+  res.render('newuser', { title: 'Create Your Account' });
+});
+
 module.exports = router;
