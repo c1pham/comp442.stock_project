05b2ba21b2139486198b63ed40410b49a49db0a2
haroldle <datthanh2401@gmail.com>
2019-11-10 14:10:58 -0500

    Install pakages that are required to use google oauth2.0. The oauth2.0 is working but it does not interacts well with database. Need time to look into database to resolve the problem. Have not commented the code but it will be done soongit status!

--- Stock_Project/app.js
+++ Stock_Project/app.js
@@ -7,11 +7,18 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const mongoose = require("mongoose");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +26,58 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+mongoose.connect("mongodb://localhost:27017/userDB", {userNewUrlParser:true});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+	email:String,
+	password:String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: '976106391986-4nrd6lljne84aqlpluudg92setdc187v.apps.googleusercontent.com',
+    clientSecret: '8MIJtgnOWPOdAtBM2VVqgAs6',
+    callbackURL: "http://localhost:3000/auth/google/googleLog"
+    //userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, done) {
+       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+         return done(err, user);
+       });
+  }
+));
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+app.get('/auth/google/googleLog', 
+  passport.authenticate('google', { failureRedirect: "/routes/index" }),
+  function(req, res) {
+    res.redirect('/routes/index');
+  });
+
+app.get
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
